7b219b919f5c8bb9aee4a0047d35c423d840f4ab
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-14 17:09:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor
   def initialize(attribute)
     attribute.each do |key, value|
-      Person.send(:attr_accessor, "#{key}")
+      self.class.send(:attr_accessor, "#{key}")
       self.send("#{key}=", value)
     end
   end
